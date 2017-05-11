de85db2182904b3cd28fb26a3b8c86959d30dca2
tatu13 <itki@hotmail.co.jp>
2017-05-11 14:04:56 +0900

    セッション追加

--- PHPTraining/cprogram/kariindex.php
+++ PHPTraining/cprogram/kariindex.php
@@ -1,3 +1,14 @@
+<?php
+//セッション開始
+session_start();
+//名前のセッションがもうあったらテキストボックスに設定しまーす
+if(isset($_SESSION["name"])){
+    $name = $_SESSION["name"];
+    }else{
+    //なかったら空白やで
+    $name = "";
+}
+ ?>
 <html>
 <head>
     <meta charset="utf-8" http-equiv="refresh" content="45">
@@ -6,7 +17,8 @@
 <body>
     <h1>ガバガバ掲示板プログラムくん</h1>
     <form action="" method="post">
-        <input type="text" name="hname" size="15" placeholder="ハンドルネーム">
+        <?php
+        print '<input type="text" name="hname" size="15" value="'.$name.'">'; ?>
         <input type="text" name="nai" size="50" placeholder="内容">
         <input type="submit" value="送信">
     </form>
@@ -30,6 +42,11 @@
                 $errors['nai'] = 'なんか書け';
             }else{
                 $datas = array('dt1' => $dt1 , 'dt2' => $dt2 , 'name' => $hn , 'nai' => $na);
+                //ここで書き込むついでにセッションがなかったら名前を保存する
+                //一応HNも書き換えられるように設定しよう
+                if(!isset($_SESSION["name"])||$_SESSION["name"]!=$hn){
+                    $_SESSION["name"]=$hn;
+                }
                 if($_SERVER['REQUEST_METHOD'] === 'POST'){
                     $fpa = fopen($file_path , 'a');
                     if($fpa){
@@ -51,7 +68,7 @@
     }
 
     if(($han = fopen($file_path,"r")) !== false){
-        echo "<div style='height:1000px; width:900px; overflow-y:scroll;'><table border='1'>\n";
+        echo "<div style='height:1000px; width:1200px; overflow-y:scroll;'><table border='1'>\n";
         while (($data = fgetcsv($han,1000,",")) !== false) {
             echo "\t<tr>\n";
             //forの中にあるから繰り返されてバグってたっぽい。最初にWhile内で判定して置換しておけば大丈夫。
