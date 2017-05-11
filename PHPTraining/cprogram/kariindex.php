8ca05437c75fdfe157742691637cff0ca5e01be6
tatu13 <itki@hotmail.co.jp>
2017-05-11 15:14:14 +0900

    というわけでセッションに対応しました

--- PHPTraining/cprogram/kariindex.php
+++ PHPTraining/cprogram/kariindex.php
@@ -11,25 +11,15 @@ if(isset($_SESSION["name"])){
  ?>
 <html>
 <head>
-    <meta charset="utf-8" http-equiv="refresh" content="45">
+    <meta charset="utf-8" http-equiv="refresh" content="60">
     <title>掲示板</title>
 </head>
 <body>
     <h1>ガバガバ掲示板プログラムくん</h1>
-    <form action="" method="post">
-        <?php
-        print '<input type="text" name="hname" size="15" value="'.$name.'">'; ?>
-        <input type="text" name="nai" size="50" placeholder="内容">
-        <input type="submit" value="送信">
-    </form>
-</body>
-</html>
-<?php
-    date_default_timezone_set('Asia/Tokyo');
-    $file_path = "dbb.csv";
-    if(isset($_POST['hname']))
-    {
-        if(isset($_POST['nai']))
+    <?php
+        date_default_timezone_set('Asia/Tokyo');
+        $file_path = "dbb.csv";
+        if(isset($_POST['hname']))
         {
             if(isset($_POST['nai']))
             {
@@ -38,22 +28,25 @@ if(isset($_SESSION["name"])){
                 $dt1 = date("Y/m/d");
                 $dt2 = date("H:i:s");
 
-            if($hn === "" || mb_ereg_match("\s",$hn)){
-                $errors['hname'] = '名前入れろ';
-            }elseif($na === "" || mb_ereg_match("\s",$na)){
-                $errors['nai'] = 'なんか書け';
-            }else{
-                $datas = array('dt1' => $dt1 , 'dt2' => $dt2 , 'name' => $hn , 'nai' => $na);
-                //ここで書き込むついでにセッションがなかったら名前を保存する
-                //一応HNも書き換えられるように設定しよう
-                if(!isset($_SESSION["name"])||$_SESSION["name"]!=$hn){
-                    $_SESSION["name"]=$hn;
-                }
-                if($_SERVER['REQUEST_METHOD'] === 'POST'){
-                    $fpa = fopen($file_path , 'a');
-                    if($fpa){
-                        rewind($fpa);
-                        fputcsv($fpa,$datas);
+                if($hn === "" || mb_ereg_match("\s",$hn)){
+                    $errors['hname'] = '名前入れろ';
+                }elseif($na === "" || mb_ereg_match("\s",$na)){
+                    $errors['nai'] = 'なんか書け';
+                }else{
+                    $datas = array('dt1' => $dt1 , 'dt2' => $dt2 , 'name' => $hn , 'nai' => $na);
+                    //ここで書き込むついでにセッションがなかったら名前を保存する
+                    //一応HNも書き換えられるように設定しよう
+                    if(!isset($_SESSION["name"])||$_SESSION["name"]!=$hn){
+                        $_SESSION["name"]=$hn;
+                    }
+                    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                        $fpa = fopen($file_path , 'a');
+                        if($fpa){
+                            //rewind($fpa);
+                            fputcsv($fpa,$datas);
+                        }
+                        fclose($fpa);
+                        header('Location:kariindex.php',true,303);
                     }
                 }
                 echo "<ul>";
@@ -65,6 +58,7 @@ if(isset($_SESSION["name"])){
                 echo "</ul>";
             }
         }
+
         if(($han = fopen($file_path,"r")) !== false){
             echo "<div style='height:800px; width:900px; overflow-y:scroll;'><table border='1'>\n";
             while (($data = fgetcsv($han,1000,",")) !== false) {
@@ -88,8 +82,10 @@ if(isset($_SESSION["name"])){
         fclose($han);
     ?>
     <form action="" method="post">
-        <input type="text" name="hname" size="15" placeholder="ハンドルネーム" maxlength='6'>
-        <input type="text" name="nai" size="50" placeholder="内容" maxlength="100">
+        <?php
+            print '<input type="text" name="hname" size="15" placeholder="ハンドルネーム" value="'.$name.'" maxlength="6">'
+        ?>
+        <input type="text" name="nai" size="50" placeholder="内容" maxlength="300">
         <input type="submit" value="送信">
     </form>
 </body>
