608c76c0de7eae4a292264f3f39570bdabe0cb4a
tatu13 <itki@hotmail.co.jp>
2017-05-12 15:36:26 +0900

    体裁ととのえー

--- PHPTraining/cprogram/index.php
+++ PHPTraining/cprogram/index.php
@@ -42,60 +42,64 @@ new Ajax.PeriodicalUpdater(
 	);
 </script>
 <body>
-    <img src="img/logo.png"/>
-    <div id="comment">
-        <form action="" method="post">
-            <?php
-                print '<input type="text" name="hname" placeholder="ハンドルネーム" value="'.$name.'" maxlength="6" class="textarea" id="name">'
-            ?>
-            <input type="text" name="nai" placeholder="内容" maxlength="300" class="textarea" id="naiyou">
-            <input type="submit" value="submit" id="sub">
-        </form>
-    </div>
-    <div id="err">
-    <?php
-        date_default_timezone_set('Asia/Tokyo');
-        $file_path = "dbb.csv";
-        if(isset($_POST['hname']))
-        {
-            if(isset($_POST['nai']))
+    <div id="wrapper">
+        <div id="header"/>
+            <img src="img/logo.png"/>
+        </div>
+        <div id="comment">
+            <form action="" method="post">
+                <?php
+                    print '<input type="text" name="hname" placeholder="ハンドルネーム" value="'.$name.'" maxlength="6" class="textarea" id="name">'
+                ?>
+                <input type="text" name="nai" placeholder="内容" maxlength="300" class="textarea" id="naiyou">
+                <input type="submit" value="submit" id="sub">
+            </form>
+        </div>
+        <div id="err">
+        <?php
+            date_default_timezone_set('Asia/Tokyo');
+            $file_path = "dbb.csv";
+            if(isset($_POST['hname']))
             {
-                $hn = htmlspecialchars($_POST["hname"],ENT_NOQUOTES,"UTF-8");
-                $na = htmlspecialchars($_POST["nai"],ENT_NOQUOTES,"UTF-8");
-                $dt1 = date("Y/m/d");
-                $dt2 = date("H:i:s");
-                if($hn === "" || mb_ereg_match("\s",$hn)){
-                    $errors['hname'] = '名前入れろ';
-                }elseif($na === "" || mb_ereg_match("\s",$na)){
-                    $errors['nai'] = 'なんか書け';
-                }else{
-                    $datas = array('dt1' => $dt1 , 'dt2' => $dt2 , 'name' => $hn , 'nai' => $na);
-                    //ここで書き込むついでにセッションがなかったら名前を保存する
-                    //一応HNも書き換えられるように設定しよう
-                    if(!isset($_SESSION["name"])||$_SESSION["name"]!=$hn){
-                        $_SESSION["name"]=$hn;
-                    }
-                    if($_SERVER['REQUEST_METHOD'] === 'POST'){
-                        $fpa = fopen($file_path , 'a');
-                        if($fpa){
-                            //rewind($fpa);
-                            fputcsv($fpa,$datas);
+                if(isset($_POST['nai']))
+                {
+                    $hn = htmlspecialchars($_POST["hname"],ENT_NOQUOTES,"UTF-8");
+                    $na = htmlspecialchars($_POST["nai"],ENT_NOQUOTES,"UTF-8");
+                    $dt1 = date("Y/m/d");
+                    $dt2 = date("H:i:s");
+                    if($hn === "" || mb_ereg_match("\s",$hn)){
+                        $errors['hname'] = '名前入れろ';
+                    }elseif($na === "" || mb_ereg_match("\s",$na)){
+                        $errors['nai'] = 'なんか書け';
+                    }else{
+                        $datas = array('dt1' => $dt1 , 'dt2' => $dt2 , 'name' => $hn , 'nai' => $na);
+                        //ここで書き込むついでにセッションがなかったら名前を保存する
+                        //一応HNも書き換えられるように設定しよう
+                        if(!isset($_SESSION["name"])||$_SESSION["name"]!=$hn){
+                            $_SESSION["name"]=$hn;
                         }
-                        fclose($fpa);
-                        header('Location:index.php',true,303);
+                        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                            $fpa = fopen($file_path , 'a');
+                            if($fpa){
+                                //rewind($fpa);
+                                fputcsv($fpa,$datas);
+                            }
+                            fclose($fpa);
+                            header('Location:index.php',true,303);
+                        }
                     }
+                    echo "<ul>";
+                    foreach($errors as $message){
+                        echo "<li>";
+                        echo $message;
+                        echo "</li>";
+                    }
+                    echo "</ul>";
                 }
-                echo "<ul>";
-                foreach($errors as $message){
-                    echo "<li>";
-                    echo $message;
-                    echo "</li>";
-                }
-                echo "</ul>";
             }
-        }
-    ?>
+        ?>
+        </div>
+        <div id="main"></div>
     </div>
-    <div id="main"></div>
 </body>
 </html>
